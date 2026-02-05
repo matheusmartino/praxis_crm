4b96033def7aa4dbb91c35c12cfcee901a45373e
Matheus De Martino <matheus.martino@outlook.com>
2026-02-05 20:44:10 -0300

    Add CSRF trusted origins for Azure deployment

--- config/settings/base.py
+++ config/settings/base.py
@@ -64,6 +64,10 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://praxiscrm-bpczdufkc2f3afah.brazilsouth-01.azurewebsites.net",
+]
+
 LANGUAGE_CODE = "pt-br"
 TIME_ZONE = "America/Sao_Paulo"
 USE_I18N = True
