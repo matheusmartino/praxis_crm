17eef34bc795aab5330320c997899b8493628df9
Matheus De Martino <matheus.martino@outlook.com>
2026-02-05 20:44:28 -0300

    Restrict allowed hosts in development settings

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -2,7 +2,10 @@ from .base import *  # noqa: F401,F403
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "praxiscrm-bpczdufkc2f3afah.brazilsouth-01.azurewebsites.net",
+]
+
 
 DATABASES = {
     "default": {
