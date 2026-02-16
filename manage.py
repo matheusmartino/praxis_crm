80240ae5cf3758c82df9772d439030a078401066
Matheus De Martino <matheus.martino@outlook.com>
2026-02-16 20:47:08 -0300

    ajuste variaveis ambiente

--- manage.py
+++ manage.py
@@ -5,7 +5,16 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
+    if os.getenv("ENVIRONMENT") == "PROD":
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE",
+            "config.settings.prod"
+        )
+    else:
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE",
+            "config.settings.dev"
+        )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
