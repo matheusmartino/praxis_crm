f63221a55585fe20d749751326eedc821df126ef
Matheus De Martino <matheus.martino@outlook.com>
2026-02-21 20:45:44 -0300

    Rename ENVIRONMENT_NAME to ENVIRONMENT in dev settings

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -2,7 +2,7 @@ from .base import *  # noqa: F401,F403
 
 DEBUG = True
 
-ENVIRONMENT_NAME = "DEV"
+ENVIRONMENT = "DEV"
 
 ALLOWED_HOSTS = [
     "praxiscrm-bpczdufkc2f3afah.brazilsouth-01.azurewebsites.net",
