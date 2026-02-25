878cfd6286db516e810243c214cc2fe8c7e1ca60
Matheus De Martino <matheus.martino@outlook.com>
2026-02-25 19:45:17 -0300

    ajuste settings

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -20,3 +20,15 @@ DATABASES = {
         "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": os.getenv("DB_PORT", "5432"),
+#     }
+# }
